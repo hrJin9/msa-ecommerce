1789c73f9c4c37314cd77ff33de58c7038e18e06
hyerin <bushyerin97@gmail.com>
2025-01-13 16:20:57 +0900

    feat: user-service의 get요청에 대한 header 검증 필터 추가

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,88 @@
+package com.example.apigatewayservice.filter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    @Value("${token.secret}")
+    private String secret;
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization Header.", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer", "");
+
+            if(!isJwtValid(jwt)) {
+                return onError(exchange, "JWT token is not valid.", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        }));
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue =true;
+        String subject = null;
+
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception e) {
+            returnValue = false;
+        }
+
+        if (StringUtils.isEmpty(subject)) {
+            returnValue = false;
+        }
+
+        return returnValue;
+    }
+
+    // Spring gateway는 Mono, Flux -> Spring WebFlux로 ServletRequest 등이 아닌 SeverRequest로 작동 (비동기 방식)
+    private Mono<Void> onError(ServerWebExchange exchange, String error, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        log.error(error);
+        return response.setComplete();
+    }
+
+
+}
